189f0ac97bbef74982eb421836b7f7b145b4363e
fatleader <antti.ho@mail.utoronto.ca>
2016-10-03 17:25:38 -0400

    Update randChar.c
    removed unnecessary comments, created compare function. 
    
    need to figure out how to ring.

--- randChar.c
+++ randChar.c
@@ -2,120 +2,131 @@
 #include <stdio.h>
 #include <limits.h>
 
-#define MIN 32
-#define MAX 127
+//#define MIN 32
+//#define MAX 127
+
+#define MIN 96
+#define MAX 123
+
+// fn prototypes
+void write(FILE *fp);
+void compare(FILE *shit, FILE *eng, int *end);
+unsigned int rand_interval(unsigned int min, unsigned int max);
+
+
+/*
+le main function.
+ideally we get it to write then read and sort
+*/
 
 int main ()
 {
-  // this is the unsigned int we are converting ascii
-  unsigned int convert;
-  // this is what we actually write.
-  char write;
+
   // the evidence
   FILE *fp;
-  // run forever...
-  while (1)
-  {
-    // call random function
-    convert = rand_interval(MIN, MAX);
-    // now we sift through the shit
-    // i've omitted the comments to make things more fun
+  int end;
+  // prompt
+  printf("Enter how many chars u want bby: ");
+  scanf("%d", &end);
 
-    if (convert == 32){
-      write = convert;
-      if ((fp = fopen("path/to/file", "w+"))==NULL){
-        // whoopsies! no path found!
-        perror("Invalid file path!");
-      } else{
-        // no error, write to file!
-        fputs(write, fp);
-        fclose (fp);
-        // we back out of if to while. hopefully rand_interval runs?
-        // pardon muh'logic
-        break;
-      }
-    }
-    else if (convert == 33){
-      write = convert;
-      if ((fp = fopen("path/to/file", "w+"))==NULL){
-        // whoopsies! no path found!
-        perror("Invalid file path!");
-      } else{
-        // no error, write to file!
-        fputs(write, fp);
-        fclose (fp);
-        // we back out of if to while. hopefully rand_interval runs?
-        // pardon muh'logic
-        break;
-      }
-    }
-    else if (convert == 46){
-      write = convert;
-      if ((fp = fopen("path/to/file", "w+"))==NULL){
-        // whoopsies! no path found!
-        perror("Invalid file path!");
-      } else{
-        // no error, write to file!
-        fputs(write, fp);
-        fclose (fp);
-        // we back out of if to while. hopefully rand_interval runs?
-        // pardon muh'logic
-        break;
-      }
-    }
-    else if (convert == 63){
-      write = convert;
-      if ((fp = fopen("path/to/file", "w+"))==NULL){
-        // whoopsies! no path found!
-        perror("Invalid file path!");
-      } else{
-        // no error, write to file!
-        fputs(write, fp);
-        fclose (fp);
-        // we back out of if to while. hopefully rand_interval runs?
-        // pardon muh'logic
-        break;
-      }
-    }
-    else if ((convert>64)&&(convert<91)){
-      write = convert;
-      if ((fp = fopen("path/to/file", "w+"))==NULL){
-        // whoopsies! no path found!
-        perror("Invalid file path!");
-      } else{
-        // no error, write to file!
-        fputs(write, fp);
-        fclose (fp);
-        // we back out of if to while. hopefully rand_interval runs?
-        // pardon muh'logic
-        break;
-      }
-    }
-    else if ((convert>96)&&(convert<123)){
-      write = convert;
-      if ((fp = fopen("path/to/file", "w+"))==NULL){
-        // whoopsies! no path found!
-        perror("Invalid file path!");
-      } else{
-        // no error, write to file!
-        fputs(write, fp);
-        fclose (fp);
-        // we back out of if to while. hopefully rand_interval runs?
-        // pardon muh'logic
-        break;
-      }
-    }
-    else {
-      // nice try no cigar
-      convert = rand_interval(MIN, MAX);
-    }
+  // call write.
+  write(fp, end);
+
+}
+
+void compare (FILE *shit, FILE *eng, int *end){
+  //where we store the string from
+  char *lineShit = NULL, *lineEng = NULL;
+  size_t lenShit = 0, lenEng = 0;
+
+  ssize_t readShit, readEng;
+
+  if((shit=fopen("text", "rb")==NULL)||(eng=fopen("english", "rb"))){
+    perror("Invalid file path");
   }
+
+  while ((readShit = getline(&lineShit, &lenShit, shit)) != -1 &&
+    (readEng = getline(&lineEng, &lenEng, eng)) != -1){
+      if ()
+    }
 
 }
 
 /*
- * a researched solution to c's bounded random generator. cannot just use %
- * since makes assumption about c's rand generator.
+write some chars
+takes in a file struct
+*/
+
+void write(FILE *fp, int *end){
+
+  // this is what we actually write.
+  unsigned int write;
+  int wLimit = rand_interval(3,27), count = 0, wcCount = 0, end;
+
+  while (count<end)
+  {
+    // call random function
+    write = rand_interval(MIN, MAX);
+
+    // for null byte.
+    if (write == 10){
+      if ((fp = fopen("text", "ab+"))==NULL){
+        // whoopsies! no path found!
+        perror("Invalid file path!");
+      } else{
+        // no error, write to file!
+        fputc(write, fp);
+        fclose (fp);
+        // we back out of if to while. hopefully rand_interval runs?
+        // pardon muh'logic
+        wcCount = 0;
+        wLimit = rand_interval(3,27);
+        write = rand_interval(MIN, MAX);
+        count += 1;
+        //break;
+      }
+    }
+
+    // just for lowercase letters.
+    if ((write>96)&&(write<123)){
+
+      //printf("wCCount: %i", wcCount);
+      //printf("wLimit: %i", wLimit);
+
+      if ((fp = fopen("text", "ab+"))==NULL){
+        // whoopsies! no path found!
+        perror("Invalid file path!");
+      }
+      else{
+        // largest shakespeare word is 27 letters: long 'nough for me.
+        if (wcCount < wLimit){
+        // now we continue, write character.
+        fputc(write, fp);
+        // we back out of if to while. hopefully rand_interval runs?
+        // pardon muh'logic
+        wcCount += 1;
+        count += 1;
+        }
+        else {
+        // write new line, reset counter, limit.
+        fputc(10, fp);
+        wLimit = rand_interval(3,27);
+        wcCount = 0;
+        }
+      }
+    }
+    else{
+      // nice try no cigar
+      write = rand_interval(MIN, MAX);
+    }
+    fclose (fp);
+}
+
+}
+
+/*
+a researched solution to c's bounded random generator. cannot just use %
+since makes assumption about c's rand generator.
  */
 unsigned int rand_interval(unsigned int min, unsigned int max)
 {
